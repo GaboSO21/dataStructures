e27941a7a35ee1fedea9d4b47b3d7f92ee98ab08
GaboSO21 <gso2090@gmail.com>
2022-09-23 08:10:14 -0600

    Preparar clase.

--- Clases/Clase_1/src/main/java/com/clase_1/controller/Clase_1.java
+++ Clases/Clase_1/src/main/java/com/clase_1/controller/Clase_1.java
@@ -5,8 +5,6 @@
 
 package com.clase_1.controller;
 
-import javax.swing.JOptionPane;
-
 /**
  *
  * @author GabrielPC
@@ -14,19 +12,6 @@ import javax.swing.JOptionPane;
 public class Clase_1 {
 
     public static void main(String[] args) {
-       
-        int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero que desea invertir:"));
-        invertirNum(num);
-    }
-
-
-    public static void invertirNum(int num){
-        if(num<10){
-            System.out.print(num);
-        }else{
-            System.out.print(num%10);
-            num /= 10;
-            invertirNum(num);
-        }
+        System.out.println("Hello World!");
     }
 }
