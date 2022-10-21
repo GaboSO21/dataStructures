08a7e5eb33f56677ca25a4906350d3ffd6ed0f22
GaboSO21 <gso2090@gmail.com>
2022-10-21 10:08:48 -0600

    Clase_6

---
+++ Clases/Clase_6/src/main/java/Lista/Lista.java
@@ -0,0 +1,71 @@
+package Lista;
+
+import javax.swing.JOptionPane;
+
+public class Lista {
+
+    private Nodo inicio;
+
+    public Lista() {
+        this.inicio = null;
+    }
+
+    public boolean isEmpty() {
+        if (inicio == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void agregar() {
+        Vehiculo v = new Vehiculo();
+        v.setMarca(JOptionPane.showInputDialog(null, "Ingrese la marca del vehiculo: ", "Marca", 3));
+        v.setModelo(
+                Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el modelo del vehiculo: ", "Modelo", 3)));
+
+        Nodo nuevo = new Nodo();
+        nuevo.setDato(v);
+
+        if (isEmpty()) {
+            inicio = nuevo;
+        } else if (v.getModelo() < inicio.getDato().getModelo()) {
+            nuevo.setSiguiente(inicio);
+            inicio = nuevo;
+        } else if (inicio.getSiguiente() == null) {
+            inicio.setSiguiente(nuevo);
+        } else {
+            Nodo aux = inicio;
+            while (aux.getSiguiente() != null && aux.getSiguiente().getDato().getModelo() < v.getModelo()) {
+                aux = aux.getSiguiente();
+            }
+            nuevo.setSiguiente(aux.getSiguiente());
+            aux.setSiguiente(nuevo);
+        }
+    }
+
+    public void extraer() {
+        if (!isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Elemento extraido: " + inicio.getDato().toString(), "Extraer", 1);
+            inicio = inicio.getSiguiente();
+        } else {
+            JOptionPane.showMessageDialog(null, "Elemento no extraido, lista vacia.", "Extraer", 0);
+        }
+    }
+
+    public String toString() {
+        String datos = "";
+        if (!isEmpty()) {
+            Nodo aux = inicio;
+            while (aux != null) {
+                datos += aux.getDato().toString() + " || ";
+                aux = aux.getSiguiente();
+            }
+            JOptionPane.showMessageDialog(null, "Lista:\n" + datos, "toString", 1);
+        } else {
+            JOptionPane.showMessageDialog(null, "No se pudo mostrar, lista vacia.", "toString", 0);
+        }
+        return datos;
+    }
+
+}
---
+++ Clases/Clase_6/src/main/java/Menu/Menu.java
@@ -0,0 +1,40 @@
+package Menu;
+
+import javax.swing.JOptionPane;
+
+import Lista.Lista;
+
+public class Menu {
+
+    private int op;
+    private Lista lista = new Lista();
+
+    public void mostrar() {
+
+        op = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "Menu Principal\n" +
+                        "1. Agregar elemento\n" +
+                        "2. Extraer elemento\n" +
+                        "3. Mostrar elemento\n" +
+                        "4. Salir\n",
+                "Menu principal", JOptionPane.QUESTION_MESSAGE));
+        switch (op) {
+            case 1:
+                lista.agregar(); 
+                mostrar();
+            case 2:
+                lista.extraer();
+                mostrar();
+            case 3:
+                lista.toString();
+                mostrar();
+            case 4:
+                System.exit(0);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Opcion invalida!", "Error", JOptionPane.ERROR_MESSAGE);
+                mostrar();
+        }
+
+    }
+}
